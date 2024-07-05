652bf6a134a828f2e75937cbd25de0fa26507b2e
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-07-05 20:43:36 +0000

    Fix reference to Python function

--- test.jl
+++ test.jl
@@ -5,4 +5,4 @@ sos_read = pyimport("sos_read")
 
 bucket_key = "confluence-dev1-sos/unconstrained/0000"
 sos_filepath = "/tmp/na_sword_v16_SOS_priors.nc"
-download_sos(bucket_key, sos_filepath)
+sos_read.download_sos(bucket_key, sos_filepath)
